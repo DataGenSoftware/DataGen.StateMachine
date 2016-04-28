d3d5f028d8dbc1ac584e6dbd55b2f25753b0b568
Karol Habczyk <karol.habczyk@gmail.com>
2016-04-28 22:18:30 +0200

    Example2

---
+++ DataGen.StateMachine.Example2/StateMachineEngine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGen.StateMachine.Example2
+{
+    public class StateMachineEngine : StateMachineEngine<States, Transitions?>
+    {
+        protected override IDictionary<StateTransition<States, Transitions?>, States> InitStatesTransitionsDictionary()
+        {
+            return new Dictionary<StateTransition<States, Transitions?>, States>
+            {
+                { new StateTransition<States, Transitions?>() { State = States.Stopped, Transition = Transitions.Play }, States.Playing},
+                { new StateTransition<States, Transitions?>() { State = States.Playing, Transition = Transitions.Stop }, States.Stopped},
+                { new StateTransition<States, Transitions?>() { State = States.Playing, Transition = Transitions.Pause }, States.Paused},
+                { new StateTransition<States, Transitions?>() { State = States.Paused, Transition = Transitions.Play }, States.Playing},
+                { new StateTransition<States, Transitions?>() { State = States.Paused, Transition = Transitions.Stop }, States.Stopped},
+            };
+        }
+    }
+}
---
+++ DataGen.StateMachine.Tests/StateMachineExample2Test.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataGen.StateMachine.Example2;
+
+namespace DataGen.StateMachine.Tests
+{
+    [TestClass]
+    public class StateMachineExample2Test
+    {
+        private static IStateMachineEngine<States, Transitions?> stateMachine;
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            stateMachine = new StateMachineEngine();
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            
+        }
+
+        [TestMethod]
+        public void StateMachineExample2_HandleStateTransition_StoppedPlay_ReturnsPlaying()
+        {
+            var currentState = States.Stopped;
+            var transition = Transitions.Play;
+            var expectedState = States.Playing;
+
+            this.AssertHandleStateTransitionResult(currentState, transition, expectedState);
+        }
+
+        [TestMethod]
+        public void StateMachineExample2_HandleStateTransition_PlayingStop_ReturnsStopped()
+        {
+            var currentState = States.Playing;
+            var transition = Transitions.Stop;
+            var expectedState = States.Stopped;
+
+            this.AssertHandleStateTransitionResult(currentState, transition, expectedState);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidStateTransitionException))]
+        public void StateMachineExample2_HandleStateTransition_StoppedPause_ThrowsInvalidStateTransitionException()
+        {
+            this.ExpectHandleStateTransitionException(States.Stopped, Transitions.Pause);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UnspecifiedTransitionException))]
+        public void StateMachineExample2_HandleStateTransition_StoppedNull_ThrowsNullTransitionException()
+        {
+            this.ExpectHandleStateTransitionException(States.Stopped, null);
+        }
+
+        private void AssertHandleStateTransitionResult(States cuttentState, Transitions transition, States expectedState)
+        {
+            StateMachineContext<States, Transitions?> stateMachineContext = this.MakeStateMachineContext(cuttentState, transition);
+
+            stateMachine.HandleStateTransition(stateMachineContext);
+
+            Assert.AreEqual(expectedState, stateMachineContext.State);
+        }
+
+        private void ExpectHandleStateTransitionException(States cuttentState, Transitions? transition)
+        {
+            StateMachineContext<States, Transitions?> stateMachineContext = this.MakeStateMachineContext(cuttentState, transition);
+
+            stateMachine.HandleStateTransition(stateMachineContext);
+        }
+
+        private StateMachineContext<States, Transitions?> MakeStateMachineContext(States cuttentState, Transitions? transition)
+        {
+            var stateMachineContext = new StateMachineContext<States, Transitions?>(new StateMachineEngine(), cuttentState);
+            stateMachineContext.Transition = transition;
+            return stateMachineContext;
+        }
+
+    }
+}
